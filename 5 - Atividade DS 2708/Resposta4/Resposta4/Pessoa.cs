e572f3dc540e98c4884c7fcd8126ec90ac3af4a0
Pedro Campos Novais <62915258+pedroCN0@users.noreply.github.com>
2021-05-10 17:26:31 -0300

    Add files via upload

---
+++ 5 - Atividade DS 2708/Resposta4/Resposta4/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resposta4
+{
+    class Pessoa
+    {
+        public double IMC(double x, double k)
+        {
+            double i;
+            i = x / (k * k);
+            return i;
+        }
+    }
+}
---
+++ 5 - Atividade DS 2708/Resposta4/Resposta4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resposta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Pessoa N = new Pessoa();
+            Console.WriteLine("Digite o peso do individuo:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Agora, digite a altura do individuo:");
+            b = double.Parse(Console.ReadLine());
+            c = N.IMC(a, b);
+            Console.WriteLine("O IMC do indivíduo é: {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
